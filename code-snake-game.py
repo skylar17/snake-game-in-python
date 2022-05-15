d55bb93239d384df7c93c670f0a7dff7ef3c1c16
skylar17 <64751046+skylar17@users.noreply.github.com>
2022-05-15 03:04:11 -0400

    bottom white bar

--- code-snake-game.py
+++ code-snake-game.py
@@ -1,4 +1,4 @@
-from contextlib import redirect_stderr
+
 import pygame 
 from pygame.locals import * 
 
@@ -14,39 +14,65 @@ screen_width = w = 800
 screen_height = h = 600
 screen = pygame.display.set_mode([w, h])
 pygame.display.set_caption('Snake Game by Shivani')
-
-# my_font = pygame.font.SysFont('Times New Roman', 25, bold=True)
 #--------------------------------------------
-# Initialing Color
-RED = (255,0,0)
+# # Defining Colors
 WHITE=(255,255,255)
-BLUE=(0,0,255)
 
-#------------------
-screen.fill(WHITE)
-#------------------------------------------
-#to keep the screen open 
-def hold():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-        pygame.display.flip()
-hold()
+RED=(255,0,0)
+GREEN=(0,255,0)
+BLUE=(0,0,255)
 #---------------------------------------------------------
+screen.fill(WHITE)
+bg_img = pygame.image.load("C:/Users/shiva/OneDrive/shiva-pc/github-repos-onedrive/snake-game-in-python/images/bg-1.jpg")
+screen.blit(pygame.transform.scale(bg_img, (w,h-35)), (0,0))
+
+clock = pygame.time.Clock()
+
+my_font = pygame.font.SysFont('Times New Roman', 20, bold=True)
+def scoreinfo(score):
+    score_display = my_font.render("Your Score is: " + str(score), True, (0,0,0), BLUE)
+    screen.blit(score_display, [0, 579])
+
+
+
+
+
+
+#to keep the screen open 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+#============================bg works until here=============================================
+
+#------------------------------------------
+
 
 # Initializing playing surface
-# play_surface = pygame.display.set_mode((screen_width,screen_height-50))
+# play_surface = pygame.display.set_mode((w,h-50))
 
 
-# Drawing Rectangle
-rect = rect(300, 300, 300, 300)
-play_surface = pygame.draw.rect(screen, RED, rect,  2)
-hold()
 
-# bg_img = pygame.image.load("C:/Users/shiva/OneDrive/shiva-pc/github-repos-onedrive/snake-game-in-python/images/bg-1.jpygame")
-# play_surface.blit(pygame.transform.scale(bg_img, (0, screen_height-50), play_surface))
+# # Drawing Rectangle
+# rect = Rect( 750, 600, 750, 600)
+# play_surface = pygame.draw.rect(screen, RED, rect,  2)
+
+# # bg_img = pygame.image.load("C:/Users/shiva/OneDrive/shiva-pc/github-repos-onedrive/snake-game-in-python/images/bg-1.jpg")
+# # play_surface.blit(bg_img, (0,0))
+
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.MOUSEMOTION:
+#             running = False
+#     pygame.display.flip()
+
+
+
+
+
 
 
 
@@ -68,4 +94,4 @@ hold()
 # rect(surface, color, rect, width=2, border_radius=0, border_top_left_radius=-1, border_top_right_radius=-1, border_bottom_left_radius=-1, border_bottom_right_radius=-1)
 
 
-
+pygame.quit()
